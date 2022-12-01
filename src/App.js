ba497b7c10b050e94b2aedfc27d66f3a4841fd44
Elijah <aa1719@imperial.ac.uk>
2022-12-01 21:16:23 +0000

    Frontend: Added routes.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import Navbar from "./components/navbar/navbar";
 import RegisterInterest from "./components/register-interest/register-interest";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <Navbar />
-        <RegisterInterest
-            title={"Dark Psychology"}
-            image={require("./assets/dark-psychology.png")}
-            description={"A breif introduction to evil. Proceed with caution."}
-        />
-    </div>
+      <BrowserRouter>
+        <div className="App">
+            <Navbar />
+            <Routes>
+                <Route
+                    path="/dark-psychology"
+                    element={
+                        <RegisterInterest
+                            title={"Dark Psychology"}
+                            image={require("./assets/dark-psychology.png")}
+                            description={"A breif introduction to evil. Proceed with caution."}
+                        />
+                    }
+                />
+            </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
