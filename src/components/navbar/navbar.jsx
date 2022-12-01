c1de66e984257bc4118dd631e5816bc408ab6f8b
Elijah <aa1719@imperial.ac.uk>
2022-12-01 20:11:39 +0000

    Frontend: Added navigation bar.

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+    return (
+        <div className={"navbar"}>
+            <p className={"navbar-brand"}>
+                conventa
+            </p>
+        </div>
+    )
+};
+
+export default Navbar;
