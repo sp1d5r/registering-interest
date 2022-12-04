40e58feab9b9c53c95c37f7dda3a65eeb3020042
Elijah <aa1719@imperial.ac.uk>
2022-12-04 19:27:55 +0000

    Frontend: Added courses page.

--- src/components/courses/courses.jsx
+++ src/components/courses/courses.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./courses.css";
+import course_options from "../../course-click";
+import CourseCardLink from "./course-card/course-card";
 
 function Courses() {
     return (
-        <div>
+        <div className={"courses-div"}>
             <h1>Courses</h1>
-
+            <p>Click any of these courses to get free access tokens!</p>
+            <div className={"courses-grid"}>
+                {
+                    course_options.map((item) => {
+                        return <CourseCardLink
+                            course={item}
+                        />
+                    })
+                }
+            </div>
         </div>
     )
 }
