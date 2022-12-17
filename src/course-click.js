c84f8a83dce48a1004712fd3fe02e3b8b222e9fa
Elijah <aa1719@imperial.ac.uk>
2022-12-17 13:23:13 +0000

    Updated course click.

--- src/course-click.js
+++ src/course-click.js
@@ -3,7 +3,7 @@ const course_options = [
         route: "/dark-psychology",
         title: "Dark Psychology",
         image: require("./assets/dark-psychology.png"),
-        description: "A breif introduction to evil. Proceed with caution."
+        description: "A brief introduction to evil. Proceed with caution."
     },
     {
         route: "/seduction",
@@ -14,111 +14,111 @@ const course_options = [
     {
         route: "/manipulation",
         title: "Intro to Manipulation",
-        image: require("./assets/manipulation1.png"), //TODO
+        image: require("./assets/manipulation1.png"),
         description: "Learn the tactics of the elite, for protection or success."
     },
     {
         route: "/does-she-like-you",
         title: "Does she like you?",
-        image: require("./assets/woman-siren.png"), //TODO
+        image: require("./assets/woman-siren.png"),
         description: "Confused about if she likes you or not? You're not trying hard enough."
     },
     {
         route: "/want",
         title: "Why You Want",
-        image: require("./assets/heart.png"), //TODO
+        image: require("./assets/heart.png"),
         description: "What do you want out of life? We'll explain it too you."
     },
     {
         route: "/negotiations",
         title: "Negotiations Advanced",
-        image: require("./assets/flight.png"), //TODO
+        image: require("./assets/flight.png"),
         description: "Become a master negotiator - get what you deserve."
     },
 
     {
         route: "/deception-detection",
         title: "Deception Detection",
-        image: require("./assets/deception-2.png"), //TODO
+        image: require("./assets/deception-2.png"),
         description: "Know exactly when people are lying to you. Know why?"
     },
     {
         route: "/patient-analysis",
         title: "Patient Deception",
-        image: require("./assets/chart.png"), //TODO
+        image: require("./assets/chart.png"),
         description: "Learn how to understand your patients better and protect them from themselves."
     },
     {
         route: "/wittiness",
-        title: "Sharpen Your Witt",
-        image: require("./assets/puzzle-man.png"), //TODO
-        description: "Deep dive into witt and using it to appear more intelligent!"
+        title: "Sharpen Your Wit",
+        image: require("./assets/puzzle-man.png"),
+        description: "Deep dive into wit and using it to appear more intelligent!"
     },
     {
         route: "/steve-jobs",
         title: "Steve Jobs",
-        image: require("./assets/cast-study.png"), //TODO
+        image: require("./assets/cast-study.png"),
         description: "Learn the psychological tricks from the master in this case study!"
     },
     {
         route: "/jeffery-dahmer",
         title: "Jeffery Dahmer",
-        image: require("./assets/cast-study.png"), //TODO
+        image: require("./assets/cast-study.png"),
         description: "Learn about the psychology of this criminal to protect yourself!"
     },
     {
         route: "/expressing-emotions",
         title: "Expressing Emotions",
-        image: require("./assets/umbrella.png"), //TODO
+        image: require("./assets/umbrella.png"),
         description: "Improve your emotional intelligence by learning to express yourself."
     },
     {
         route: "/self-control",
         title: "Self Control",
-        image: require("./assets/loading-bar.png"), //TODO
+        image: require("./assets/loading-bar.png"),
         description: "Improve your self control and become a better person!"
     },
     {
         route: "/nudges",
         title: "How to Convince",
-        image: require("./assets/manipulation1.png"), //TODO
+        image: require("./assets/manipulation1.png"),
         description: "Stop. You're trying too hard. A few nudges can go a long way."
     },
     {
         route: "/recognising-danger",
         title: "Recognising Danger",
-        image: require("./assets/shield.png"), //TODO
+        image: require("./assets/shield.png"),
         description: "Understand when you might be in danger so better prepare."
     },
     {
         route: "/childhood-psychology",
         title: "Childhood Psychology",
-        image: require("./assets/flower-girl.png"), //TODO
+        image: require("./assets/flower-girl.png"),
         description: "Learn about childhood psychology to help your kids!"
     },
     {
         route: "/learning-languages",
         title: "Learning Languages",
-        image: require("./assets/manipulation2.png"), //TODO
+        image: require("./assets/manipulation2.png"),
         description: "Be better at learning languages, become more cultured!"
     },
     {
         route: "/memorisation",
         title: "Memorisation",
-        image: require("./assets/puzzle-man.png"), //TODO
+        image: require("./assets/puzzle-man.png"),
         description: "Explore the science behind memorisation, become more intelligent!"
     },
     {
         route: "/drug-psychology",
         title: "Drug Psychology",
-        image: require("./assets/manipulation2.png"), //TODO
-        description: "What happens when you're on drugs and hwo they can improve your life!"
+        image: require("./assets/manipulation2.png"),
+        description: "What happens when you're on drugs and how they can improve your life!"
     },
     {
         route: "/patience",
         title: "Patience",
-        image: require("./assets/hourglass.png"), //TODO
-        description: "Patience is a viture... Not one that you possess. (yet)"
+        image: require("./assets/hourglass.png"),
+        description: "Patience is a virtue... Not one that you possess. (yet)"
     },
 ]
 
