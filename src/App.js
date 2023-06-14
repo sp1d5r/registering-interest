fd66f9c975b623f12d3b2e9c935742a992abf842
Elijah <aa1719@imperial.ac.uk>
2023-06-14 19:36:43 +0100

    Frontend: Cleaned frontend.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     return (
       <BrowserRouter>
         <div className="App">
-            {/*<ScrollingBanner />*/}
+            <ScrollingBanner />
             <Routes>
                 <Route
                     path="/"
--- src/components/home/spotlight-mask/SpotlightMask.jsx
+++ src/components/home/spotlight-mask/SpotlightMask.jsx
@@ -4,7 +4,7 @@ import RegisterInterest from "../../register-interest/RegisterInterest";
 
 const SpotlightMask = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [coordinates, setCoordinates] = useState({ x: '50%', y: '50%' });
+    const [coordinates, setCoordinates] = useState({ x: '80%', y: '20%' });
     const [buttonAnimation, setButtonAnimation] = useState(false);
     const [email, setEmail] = useState("");
 
@@ -42,25 +42,47 @@ const SpotlightMask = () => {
     };
 
     return (
-        <div style={{position: 'relative'}}>
-            <section className={"behind-div"} hidden={isOpen} style={{backgroundColor: "white"}}>
-                <RegisterInterest isPrimary={true} handleInputChange={handleInputChange} email={email}/>
-                <span className={`hover-btn ${buttonAnimation ? 'animate' : ''}`} onClick={handleClick}>
+        <div style={{position: 'relative', zIndex: 0}}>
+            <section className={"behind-div"} hidden={isOpen}>
+                <RegisterInterest
+                    isPrimary={true}
+                    handleInputChange={handleInputChange}
+                    email={email}
+                    continueButton={<span className={`hover-btn ${buttonAnimation ? 'animate' : ''} ${isOpen ? 'open' : 'closed '}`} onClick={handleClick}>
                     <svg viewBox="0 0 62 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M61.0607 13.0607C61.6465 12.4749 61.6465 11.5251 61.0607 10.9393L51.5147 1.3934C50.9289 0.807612 49.9792 0.807612 49.3934 1.3934C48.8076 1.97918 48.8076 2.92893 49.3934 3.51472L57.8787 12L49.3934 20.4853C48.8076 21.0711 48.8076 22.0208 49.3934 22.6066C49.9792 23.1924 50.9289 23.1924 51.5147 22.6066L61.0607 13.0607ZM0 13.5H60V10.5H0V13.5Z" />
                     </svg>
-                </span>
+                </span>}
+                />
             </section>
             <section
                 className={`overlay ${isOpen ? 'is-open' : ''}`}
                 style={{ '--x': coordinates.x, '--y': coordinates.y}}
             >
-                <RegisterInterest isPrimary={false} handleInputChange={handleInputChange} email={email}/>
-                <span className="hover-btn2 " onClick={handleClick}>
+                {!isOpen ?
+                    <RegisterInterest isPrimary={false} handleInputChange={handleInputChange} email={email} continueButton={<span className="hover-btn2 " onClick={handleClick}>
                   <svg viewBox="0 0 62 24" fill="black" xmlns="http://www.w3.org/2000/svg">
                         <path fill="black" d="M61.0607 13.0607C61.6465 12.4749 61.6465 11.5251 61.0607 10.9393L51.5147 1.3934C50.9289 0.807612 49.9792 0.807612 49.3934 1.3934C48.8076 1.97918 48.8076 2.92893 49.3934 3.51472L57.8787 12L49.3934 20.4853C48.8076 21.0711 48.8076 22.0208 49.3934 22.6066C49.9792 23.1924 50.9289 23.1924 51.5147 22.6066L61.0607 13.0607ZM0 13.5H60V10.5H0V13.5Z" />
                     </svg>
-                </span>
+                </span>}/> :
+                    <>
+                        <div className={"thank-you-div"}>
+                            <h1 className={"thank-you-title"}>Thank you!</h1>
+                            <p className={"thank-you-body"}>
+                                Well done on taking the initiative and registering to conventa.
+                            </p>
+
+                            <p className={"thank-you-body"}>
+                                We promise to teach you the lessons required to improve your personal and
+                                professional life.
+                            </p>
+
+                            <b className={"thank-you-body"}>
+                                When we're ready to help - we'll email you
+                            </b>
+                        </div>
+                    </>
+                }
             </section>
         </div>
     );
